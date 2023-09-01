ae76643b85710ada31acaefc5971b1eeebbfe631
Robert Berg <raberg1@gmail.com>
2023-09-01 10:51:51 -0500

    added to notes 8/31

--- a1_writeup.md
+++ a1_writeup.md
@@ -21,3 +21,4 @@ Mr. Berg will look at all your files to determine what you have completed.  If t
 
 
 3. What are some things that you learned through this assignment?
+
--- notes.py
+++ notes.py
@@ -5,8 +5,8 @@
 # types
 
 # str
-y = "I love Computer Science!"
-
+y: str = "I love Computer Science!"
+print(y)
 # numbers
 mynum = 47
 anothernum = 47.7
@@ -16,86 +16,87 @@ print(type(anothernum))
 # bools - True, False
 flag = True
 print(flag)
+print(type(flag))
 
 # lists - can hold a list of items of any type
-lane_tech = ["chicago", "high school", 1908, 60618, "addison/western"]
-print(lane_tech.reverse())
+lane_tech = ["chicago", "high school", 1908, 60618, "addison/western", False]
+# print(lane_tech.reverse())
 print(lane_tech)
 z = lane_tech.pop()
 print(z)
 
 
-# Indices
-print(lane_tech[1])
-print(lane_tech[-1])
-print(lane_tech[len(lane_tech)-1])
+# # Indices
+# print(lane_tech[1])
+# print(lane_tech[-1])
+# print(lane_tech[len(lane_tech)-1])
 
-# Slices
-print(lane_tech[1:3])
-print(lane_tech[:3])
-print(lane_tech[1:])
-print(lane_tech[:])
+# # Slices
+# print(lane_tech[1:3])
+# print(lane_tech[:3])
+# print(lane_tech[1:])
+# print(lane_tech[:])
 
-# Functions
+# # Functions
 
-# defining a function
-def hello_world():
-    print("hello world")
+# # defining a function
+# def hello_world():
+#     print("hello world")
 
-# calling a function
-hello_world()
+# # calling a function
+# hello_world()
 
-def add_two(n: int):
-    """Takes a number and returns that number + 2
+# def add_two(n: int):
+#     """Takes a number and returns that number + 2
     
-    Args: 
-        n - a number
+#     Args: 
+#         n - a number
     
-    Returns:
-        the input number + 2
-    """
-    return n + 2
+#     Returns:
+#         the input number + 2
+#     """
+#     return n + 2
 
-assert add_two(5) == 7, "add_two with input 5 test"
-assert add_two(-2) == 0, "add_two with input -2 test"
-assert add_two(0) == 2, "add_two with input 0 test"
+# assert add_two(5) == 7, "add_two with input 5 test"
+# assert add_two(-2) == 0, "add_two with input -2 test"
+# assert add_two(0) == 2, "add_two with input 0 test"
 
-# Loops
+# # Loops
 
-# For Loop Template 1
-# do something for each item in a list
-lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-for el in lst:
-    print(el)
+# # For Loop Template 1
+# # do something for each item in a list
+# lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# for el in lst:
+#     print(el)
 
-# For Loop Template 2
-# do something n times
-for i in range(5):
-    print("Intro to AI")
+# # For Loop Template 2
+# # do something n times
+# for i in range(5):
+#     print("Intro to AI")
 
-# For Loop Template 3
-# do something for each item in a list but we care about the index
-for r in range(len(lst)):
-    if lst[r] == 'b':
-        lst[r] = 'c'
+# # For Loop Template 3
+# # do something for each item in a list but we care about the index
+# for r in range(len(lst)):
+#     if lst[r] == 'b':
+#         lst[r] = 'c'
 
-print(lst)
+# print(lst)
 
-# Dictionaries
+# # Dictionaries
 
-dict = {"name": "rob", "age": 30}
-print(dict["age"])
+# dict = {"name": "rob", "age": 30}
+# print(dict["age"])
 
-# Random
-import random
+# # Random
+# import random
 
-food = ['pizza', 'carrots', 'eggs']
-dinner = random.choice(food)
-print(dinner)
+# food = ['pizza', 'carrots', 'eggs']
+# dinner = random.choice(food)
+# print(dinner)
 
-# f String
-age = 25
-# print("Mary is " + age + " years old")
-print(f"Mary is {age} years old")
+# # f String
+# age = 25
+# # print("Mary is " + age + " years old")
+# print(f"Mary is {age} years old")
 
 

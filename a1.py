633e205b33c35902ac17cb0aee919e49f2401530
Robert Berg <raberg1@gmail.com>
2023-09-11 11:12:27 -0500

    finished sum_list for real this time

--- a1.py
+++ a1.py
@@ -78,7 +78,10 @@ def sum_list(lst: List[int]) -> int:
     Returns:
         the sum of the passed in list
     """
-    raise NotImplementedError("sum_list")
+    s = 0
+    for el in lst:
+        s += el
+    return s
 
 
 def mean(lst: List[int]) -> float:
